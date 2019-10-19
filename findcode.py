11dec745effe4d227e5daaa26015a250b989a084
Brad Schick <schickb@gmail.com>
2019-10-19 12:13:49 -0700

    minor cleanup

--- findcode.py
+++ findcode.py
@@ -19,54 +19,56 @@ LABELS = complete(bytes([0x30,0x30,0x32]))
 BREAK = complete(bytes([0x30,0x37,0x30,0x5E]))
 
 
-def eat(ser):
-   while True:
-      result = ser.readline()
-      if not result:
-         break
-
-def wait(ser):
-   while True:
-      result = ser.readline()
-      if result[:3] == bytes('500','ascii'):
-         break
-      elif result[:3] == bytes('501','ascii'):
-         print('!! bad checksum')
-      elif result[:3] == bytes('502','ascii'):
-         print(f'!! error: {result[3:6]}')
-
-def is_code(ser):
-    while True:
-      result = ser.readline()
-      if not result or 'Invalid Access  Code' in result.decode('ascii'):
-         return False
-      elif 'Enter Section   ---' in result.decode('ascii'):
-         return True 
-
-def press(ser, key):
-   command = complete(bytes([0x30,0x37,0x30,ord(key)]))
-   ser.write(command)
-   ser.flush()
-   wait(ser)
-   ser.write(BREAK)
-   ser.flush()
-   wait(ser)
-
 with serial.Serial(SERIALDEVICE, baudrate=9600, timeout=2) as ser:
-   print("clearing")
-   eat(ser)
+
+   def eat():
+      while True:
+         result = ser.readline()
+         if not result:
+            break
+
+   def wait():
+      while True:
+         result = ser.readline()
+         if result:
+            if result[:3] == bytes('500','ascii'):
+               break
+            elif result[:3] == bytes('501','ascii'):
+               print('!! bad checksum')
+            elif result[:3] == bytes('502','ascii'):
+               print(f'!! error: {result[3:6]}')
+
+   def is_code():
+       while True:
+         result = ser.readline()
+         if not result or 'Invalid Access  Code' in result.decode('ascii'):
+            return False
+         elif 'Enter Section   ---' in result.decode('ascii'):
+            return True 
+
+   def press(key):
+      command = complete(bytes([0x30,0x37,0x30,ord(key)]))
+      ser.write(command)
+      ser.flush()
+      wait()
+      ser.write(BREAK)
+      ser.flush()
+      wait()
+
+   print("clearing response buffer")
+   eat()
 
    for code in range(0, 10000):
       guess = f'{code:04}'
-      press(ser, '#')
-      press(ser, '*')
-      press(ser, '8')
-      for i in range(4):
-         press(ser, guess[i])
-      if is_code(ser):
-         print(f'{code} is the code')
+      press('#')
+      press('*')
+      press('8')
+      for c in guess:
+         press(c)
+
+      if is_code():
+         print(f'{guess} is the code')
          break
       else:
-         print(f'{code} is invalid')
-         eat(ser)
-
+         print(f'{guess} is invalid')
+         eat()
